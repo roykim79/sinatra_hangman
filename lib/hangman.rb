86e71622fcb894387497212111e61f5fe72e4358
Roy Kim <rykim79@gmail.com>
2018-08-29 14:56:34 -0400

    add #guess()

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,9 +1,21 @@
 class Hangman
   attr_accessor :word, :guesses_left, :show_word
 
-  def initialize(word = "hollywood")
+  def initialize(word)
     @word = word
     @guesses_left = 5
     @show_word = '_' * @word.length
+  end
+
+  def guess(letter)
+    if @word.include?(letter)
+      @word.chars.each_with_index do |char, index|
+        if char == letter
+          @show_word[index] = letter
+        end
+      end
+    else
+      @guesses_left -= 1
+    end
   end
 end
--- spec/hangman_spec.rb
+++ spec/hangman_spec.rb
@@ -13,4 +13,17 @@ describe Hangman do
       expect(@game.show_word()).to eq('_____')
     end
   end
+
+  describe '#guess' do
+    it 'it decrements @guesses_left with a wrong guess' do
+      @game.guess('a')
+      expect(@game.guesses_left()).to eq(4)
+    end
+
+    it 'replaces the _ with the guessed letter if it is part of the word' do
+      @game.guess('s')
+      expect(@game.guesses_left()).to eq(5)
+      expect(@game.show_word()).to eq('s____')
+    end
+  end
 end
